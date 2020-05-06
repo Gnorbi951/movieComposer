0a1bf78a01d0f06b28bc7d576e6d2dfcf98baa05
David <davmmm@gmail.com>
2020-05-06 03:46:36 +0200

    Add file event watcher

---
+++ watcher.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+inotifywait -m $HOME/bin/ -e create -e moved_to -e modify |
+    while read dir action file; do
+        echo "The file '$file' appeared in directory '$dir' via '$action'";
+        if [[ $file == *".mp4" ]]; then # Does the file end with .mp4?
+            echo "mp4 file" # If so, do your thing here!
+        fi
+        # do something with the file
+    done
