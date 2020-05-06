12d5c5614802ba7807082d259bd47bfd179fd56f
David <davmmm@gmail.com>
2020-05-06 04:14:37 +0200

    Reorganise project structure

--- join_movies.sh
+++ join_movies.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 #transcoding
-ffmpeg -i input2.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts temp1.ts
-ffmpeg -i input1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts temp2.ts
+ffmpeg -i input/input2.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts temp/temp1.ts
+ffmpeg -i input/input1.mp4 -c copy -bsf:v h264_mp4toannexb -f mpegts temp/temp2.ts
 
 #joining
-ffmpeg -i "concat:temp1.ts|temp2.ts" -c copy -bsf:a aac_adtstoasc output.mp4 -y
+ffmpeg -i "concat:temp/temp1.ts|temp/temp2.ts" -c copy -bsf:a aac_adtstoasc output/output.mp4 -y
 
 #removing temp file
-rm temp1.ts
-rm temp2.ts
+rm temp/*
--- watcher.sh
+++ watcher.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-inotifywait -m $HOME/bin/ -e create -e moved_to -e modify |
+inotifywait -m ./input/ -e create -e moved_to -e modify |
     while read dir action file; do
         echo "The file '$file' appeared in directory '$dir' via '$action'";
         if [[ $file == *".mp4" ]]; then # Does the file end with .mp4?
